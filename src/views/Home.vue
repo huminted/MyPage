aa355467beed9aa8d72164866fe5b3f9ddf7722f
humin <humin.ted@gmail.com>
2020-11-08 21:28:18 +0800

    更新图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,14 @@
     </div>
     <div class="title-desc-container">
       <a id="github" href="https://github.com/huminand" target="_blank" rel="noopener noreferrer"
-        ><img class="title-font" src="./../assets/images/github.svg" />Developer</a
+        ><img class="title-font" src="./../assets/images/as.svg" />Developer</a
       >
       <span>/</span>
-      <a id="sspai" href="https://sspai.com/u/spencerwoo/posts" target="_blank" rel="noopener noreferrer"
-        ><img class="title-font" src="./../assets/images/github.svg" />Writer</a
+      
+      <a id="github" href="https://www.douban.com/people/huminand/" target="_blank" rel="noopener noreferrer"
+        ><img class="title-font" src="./../assets/images/douban.svg" />Douban</a
       >
+     
       <span>/</span>
       <a id="blog" href="https://www.zhihu.com/people/humin" target="_blank" rel="noopener noreferrer"
         ><img class="title-font" src="./../assets/images/zeit.svg" />Zhihu</a
@@ -21,25 +23,28 @@
       <h2>WHO AM I</h2>
       <p>
         I'm <i>Humin</i> with the ID <i>Huminand</i>. A <i>Android Software Enginner</i> Based in Beijing
-      <p>Work for<i>  <img class="title-font" src="./../assets/images/baidu.svg" /> Baidu Zhidao 1 Year</i> <p>
+      <p>Work for <i><img class="title-font" src="./../assets/images/baidu.svg" /> Baidu Zhidao 1 Year</i> <p>
         Some other variations:
         <a href="https://twitter.com/humin147" target="_blank" rel="noopener noreferrer">@Humin147</a> /
         <a href="https://weibo.com/win8humin" target="_blank" rel="noopener noreferrer">@Huminand</a> /
         <a href="https://github.com/huminand" target="_blank" rel="noopener noreferrer">@Huminand</a>. 
-        You may know me from GitHub / Weibo / Twitter or other places. Yes, those are also me.
+        You may know me from 
+        <img class="title-font" src="./../assets/images/gihub.svg" />GitHub / 
+         <img class="title-font" src="./../assets/images/weibo.svg" />Weibo / 
+         <img class="title-font" src="./../assets/images/twitter.svg" />Twitter or other places. Yes, those are also me.
       </p>
 
-      <h2>Academics</h2>
+      <h2>Programming</h2>
       <ul>
         <li>🔭 <b>Fields of interest:</b></li>
           <ul>
              <li>Programming: 
+               <img class="title-font" src="./../assets/images/java.svg" />Java / 
                <img class="title-font" src="./../assets/images/android.svg" />Android / 
                <img class="title-font" src="./../assets/images/kotlin.svg" />Kotlin / 
                <img class="title-font" src="./../assets/images/kotlin.svg" />JS  </li> 
-             <li>Singer: Jacky Cheung  </li> 
+             
           </ul>
-
       </ul>
 
       <h2>Devices</h2>
@@ -49,11 +54,21 @@
       </ul>
       <h2>Tools</h2>
       <ul>
-            <li> <a><img class="title-font" src="./../assets/images/as.svg" /> Android Studio</a>  </li>
-            <li> <a><img class="title-font" src="./../assets/images/idea.svg" />IDEA</a>  </li>
-            <li> <a><img class="title-font" src="./../assets/images/vsc.svg" /> Visual Studio</a>  </li>
- 
+            <li> <img class="title-font" src="./../assets/images/as.svg" />Android Studio</li>
+            <li> <img class="title-font" src="./../assets/images/idea.svg" />IDEA</li>
+            <li><img class="title-font" src="./../assets/images/vsc.svg" /> Visual Studio</li>
+            <li><img class="title-font" src="./../assets/images/typora.svg" />Typora</li>
       </ul>
+
+      <h2>Music</h2>
+      <ul>
+        <li>Jacky Cheung</li>
+        <li><img class="title-font" src="./../assets/images/applemusic.svg" /></li>
+         <li><img class="title-font" src="./../assets/images/spotify.svg" /></li>
+          <li><img class="title-font" src="./../assets/images/doubanfm.svg" /></li>
+
+      </ul>
+
       <h2>Contacts</h2>
       <ul>
         <li>
