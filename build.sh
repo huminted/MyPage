5ac78c3ee5d3c904d737d70d767aa4647148cf6e
huminand <humin147@live.com>
2021-03-06 22:14:15 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "update"
 git push
-ssh root@iwakeup.cn "cd /home/code/MyPage;git pull;yarn install;yarn build;rm -rf /home/apps/MyPage/* ;cp -a ./dist/* /home/apps/MyPage/;"
+ssh root@iwakeup.cn "cd /home/code/MyPage;git pull;yarn install;yarn build;rm -rf /home/apps/MyPage/* ;cp -a ./dist/* /home/apps/MyPage;"
