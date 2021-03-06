1e3a72d17a535632313e64695ae42a20c1fe211e
huminand <humin147@live.com>
2021-03-06 22:18:18 +0800

    update

--- runDocker.sh
+++ runDocker.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:8080 -v /home/apps/MyPage:/usr/local/tomcat/webapps/ROOT tomcat:withapps 
+docker run --rm -d -p 80:8080 -v /home/apps/MyPage:/usr/local/tomcat/webapps/ROOT tomcat:withapps 
